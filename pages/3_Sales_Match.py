9f8df201380096f301bf4d2800da06e67e1fe0a3
Sergio Lopez <129771463+Ser911af@users.noreply.github.com>
2025-09-09 15:22:19 -0500

    Update 3_Sales_Match.py

--- pages/3_Sales_Match.py
+++ pages/3_Sales_Match.py
@@ -1,9 +1,9 @@
-# pages/0_Explorer.py
+
 import streamlit as st
 from simple_auth import ensure_login, current_user, logout_button
 
-st.set_page_config(page_title="Explorer", page_icon="🔎", layout="wide")
-st.title("🔎 Explorer")
+st.set_page_config(page_title="Sales Match", page_icon="🔎", layout="wide")
+st.title("🔎 Better Match")
 
 user = ensure_login()  # corta la ejecución si no hay login
 
